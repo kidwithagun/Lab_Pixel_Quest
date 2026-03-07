d2fe8e7bc6dc0ba68da928a0067f37b44f7a1675
kidwithagun <andresbito09@gmail.com>
2026-03-07 16:29:16 -0500

    End of Lesson 4
    I made multiple scenes and code to manipulate the player, objects, their respective components, and change between scenes.

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,40 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    string variable1 = "My ";
-    private int aNumber = 3;
+    //Player movement
+    private Rigidbody2D rb2d;
+    public int ySpeed = 5;
+    public int xSpeed = 5;
+
+    //Player color changer
+    private SpriteRenderer sr;
+
+    //Level changer
+    public string nextLevel;
+
+    //Start is called once when script is first activated
     void Start()
     {
-        string variable2 = "First ";
-        string variable3 = "Script";
-        Debug.Log(variable1 + variable2 + variable3);
+        //Player movement
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //Player color
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(aNumber++);
+        //Player movement
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        rb2d.velocity = new Vector2(xInput * xSpeed, yInput * ySpeed);
 
-        if (Input.GetKeyDown(KeyCode.W) || (Input.GetKeyUp(KeyCode.W)))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            transform.position += new Vector3(0, 1f, 0);
+            sr.material.color = new Color32(255, 0, 0, 255);
         }
-        
-        if (Input.GetKeyUp(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            transform.position += new Vector3(-1f, 0, 0);
+            sr.material.color = new Color32(0, 255, 0, 255);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            transform.position += new Vector3(0, -1f, 0);
+            sr.material.color = new Color32(0, 0, 255, 255); ;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
         {
-            transform.position += new Vector3(1f, 0, 0);
+            case "Death":
+            {
+                string thisScene = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(thisScene);
+                break;
+            }
+            case "Finish":
+            {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+            }
         }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
     }
 }
